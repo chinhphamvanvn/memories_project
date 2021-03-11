0803e81505f68d485202a6f31f083e749cd15f21
Chinh Pham <chinhphamvanvn@gmail.com>
2021-03-11 22:14:51 +0700

    logout ok

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import { AppBar, Avatar, Typography, Toolbar, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import useStyles from './styles';
 import memories from '../../images/logo.jpg';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const classes = useStyles();
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
-    console.log(user);
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
+    
+    const logout = () => {
+        dispatch({ type: 'LOGOUT'});
+        history.push('/');
+        setUser(null);
+    }
+
     useEffect(() => {
         const token = user?.token;
         //JWT...
         setUser(JSON.parse(localStorage.getItem('profile')));
-    }, []);
-    
+    }, [location]);
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
@@ -27,7 +37,7 @@ const Navbar = () => {
                     <div className={classes.profile}>
                         <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ) : (
                     <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
